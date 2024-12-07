fb418c54d91d2766a2d5c3e1cc548e90bb1fd089
Yluxe11 <yassin.jaghiti@gmail.com>
2024-12-07 12:31:45 -0800

    Update App.tsx
    Signed-off-by: Yluxe11 <yassin.jaghiti@gmail.com>

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import { Header } from './components/layout/Header';
 import { Footer } from './components/layout/Footer';
 import { Hero } from './components/home/Hero';
@@ -6,20 +8,28 @@ import { Features } from './components/home/Features';
 import { StyleDiscovery } from './components/home/StyleDiscovery';
 import { Shop } from './pages/Shop';
 import { CustomDesign } from './pages/CustomDesign';
+import { NotFound } from './pages/NotFound';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <Header />
-      <main>
-        <Hero />
-        <Features />
-        <StyleDiscovery />
-        <Shop />
-        <CustomDesign />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+          <Header />
+        
+        <main className="container mx-auto px-4 py-8">
+                 <Routes>
+            <Route path="/" element={<Hero />} />
+            <Route path="/features" element={<Features />} />
+            <Route path="/discover-styles" element={<StyleDiscovery />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/custom-design" element={<CustomDesign />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
